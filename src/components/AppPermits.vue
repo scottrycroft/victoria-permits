4f6d7b0fb89df47b11ac0bb8c969840d404f3a44
Scott Rycroft <srycroft@freschesolutions.com>
2023-06-16 08:05:08 -0700

    Paging, tweaks, start display dialog

--- src/components/AppPermits.vue
+++ src/components/AppPermits.vue
@@ -5,28 +5,43 @@ import { FilterMatchMode } from 'primevue/api';
 import Column from 'primevue/column';
 import DataTable from 'primevue/datatable';
 import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+
 
 // @ts-ignore: seems like a TS bug?? https://github.com/microsoft/TypeScript/issues/43784
 import permitInfo from "@/permitInfo.json";
-
 const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
     primaryStreetName: { value: null, matchMode: FilterMatchMode.STARTS_WITH }
 });
 
 const formatDate = (unixDate: number) => {
-    return new Date(unixDate * 1000).toString().split(" ").slice(0,4).slice(1).join(" ");
+    return new Date(unixDate * 1000).toString().split(" ").slice(0, 4).slice(1).join(" ");
 }
 
 const filteredData = ref(permitInfo.permits);
 
 const globalFilter = ref([]);
+
+
+// Permit Dialog
+const permit = ref({});
+const permitDialogVisible = ref(false);
+const viewPermit = (permitData: any) => {
+    permit.value = { ...permitData };
+    permitDialogVisible.value = true;
+};
 </script>
 
 <template>
     <main>
-        <DataTable :value="filteredData" :rows="10" paginator width="100%" :globalFilter="globalFilter" filterDisplay="menu"
-            v-model:filters="filters" :globalFilterFields="['primaryStreetName', 'applicant', 'applicationType', 'status']">
+        <DataTable :value="filteredData" width="100%" v-model:filters="filters" :globalFilter="globalFilter"
+            filterDisplay="menu"
+            :globalFilterFields="['primaryStreetName', 'applicant', 'applicationType', 'status', 'applicationType', 'folderNumber', 'status', 'addresses', 'purpose',]"
+            :rowsPerPageOptions="[5, 10, 20, 50]" :rows="5" paginator
+            paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+            sortField="lastUpdated" :sortOrder="-1">
             <template #header>
                 <div class="flex justify-content-end">
                     <span class="p-input-icon-left">
@@ -35,6 +50,11 @@ const globalFilter = ref([]);
                     </span>
                 </div>
             </template>
+            <Column :exportable="false" style="min-width:1rem">
+                <template #body="{ data }">
+                    <Button icon="pi pi-search" outlined rounded title="View Permit" @click="viewPermit(data)" />
+                </template>
+            </Column>
             <Column field="primaryStreetName" filterField="primaryStreetName" header="Primary Street Name" :sortable="true">
             </Column>
             <Column field="applicant" header="Applicant" :sortable="true"></Column>
@@ -62,5 +82,16 @@ const globalFilter = ref([]);
                 </template>
             </Column>
         </DataTable>
+
+        <Dialog v-model:visible="permitDialogVisible" :style="{ 'min-width': '450px' }" header="Permit Details"
+            :modal="true" class="p-fluid">
+            <h3>Permit</h3>
+            <div class="">
+                <span class="p-float-label">
+                    <InputText id="primaryStreet" v-model="permit.primaryStreetName" />
+                    <label for="primaryStreet">Primary Street</label>
+                </span>
+            </div>
+        </Dialog>
     </main>
 </template>
