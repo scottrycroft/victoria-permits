0be7344834292de569585089993d601f6ecf63db
Scott Rycroft <srycroft@freschesolutions.com>
2024-02-27 07:12:20 -0800

    Trigger rebuild change - testing indexedDB

--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,11 @@ import Dexie, { type Table } from "dexie";
 
 import { type PermitsEntityDB } from "./types/Permits";
 
+// App change to trigger rebuild
+if (typeof globalThis == "string") {
+	console.log("Yes");
+}
+
 export class PermitsDB extends Dexie {
 	// 'lastSeenPermits' is added by dexie when declaring the stores()
 	// We just tell the typing system this is the case
